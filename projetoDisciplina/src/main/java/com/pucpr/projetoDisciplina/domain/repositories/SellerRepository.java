aa1a74c67e2d425f28ab43a1f96ac7ed8623cce7
LucasAuSilva <silvaaugustolucas@gmail.com>
2021-11-04 15:00:21 -0300

    novos repositorys para criacao de seller e address em produto

---
+++ projetoDisciplina/src/main/java/com/pucpr/projetoDisciplina/domain/repositories/SellerRepository.java
@@ -0,0 +1,8 @@
+package com.pucpr.projetoDisciplina.domain.repositories;
+
+import com.pucpr.projetoDisciplina.domain.entities.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
